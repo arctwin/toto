c56258f47b00d72dcaf6f34b79dcaa3a95ac9159
krad <hunt0101@163.com>
2023-08-01 20:42:38 +0800

    version2

--- MouseListener.py
+++ MouseListener.py
@@ -1,16 +1,8 @@
-from threading import Thread
 
 import pynput, time
-from pynput import keyboard
+
 
 import ReportUtil
-# # from ReportUtil import toTxt
-#
-
-
-
-
-
 
 def on_click(x, y, button, pressed):
     if button == pynput.mouse.Button.left and pressed:
